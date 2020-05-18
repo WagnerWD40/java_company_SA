e8f10b2f93c269e67dd1ee02b8e7c84e9a85bd68
WagnerWD40 <48893970+WagnerWD40@users.noreply.github.com>
2020-05-18 11:44:13 -0300

    Update BancoDeDados.java

--- src/sistema/BancoDeDados.java
+++ src/sistema/BancoDeDados.java
@@ -3,7 +3,7 @@ package sistema;
 public class BancoDeDados {
 
     public static String stringDeConexao = "jdbc:sqlserver://localhost:1433;databaseName=sistema_de_funcionarios;integratedSecurity=true";
-    public static String usuario = "DESKTOP-EOI25B1\\Wagne";
-    public static String senha = "vegita";
+    public static String usuario = "***********";
+    public static String senha = "*********";
 
 }
